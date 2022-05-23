0c2c877ae34b753c37f14aec6ca24a11807b79f4
Jiri Novotny <novotny.049@gmail.com>
2022-05-24 00:00:03 +0300

    pixel put and get

--- rtlib/src/canvas.rs
+++ rtlib/src/canvas.rs
@@ -1,5 +1,6 @@
 use crate::color::Color;
 
+#[derive(Debug, Clone)]
 pub struct Canvas {
     width: u32,
     height: u32,
@@ -13,6 +14,28 @@ impl Canvas {
             height,
             data: vec![Color::BLACK; width as usize * height as usize],
         }
+    }
+
+    fn in_bounds(&self, x: u32, y: u32) {
+        if x >= self.width && y >= self.height {
+            panic!("Point out of canvas bounds");
+        }
+    }
+
+    fn index(&self, x: u32, y: u32) -> usize {
+        (y * self.height + x) as usize
+    }
+
+    pub fn put_pixel(&mut self, x: u32, y: u32, color: Color) {
+        self.in_bounds(x, y);
+        let i = self.index(x, y);
+        self.data[i] = color;
+    }
+
+    pub fn get_pixel(&self, x: u32, y: u32) -> Color {
+        self.in_bounds(x, y);
+        let i = self.index(x, y);
+        self.data[i]
     }
 }
 
@@ -36,5 +59,17 @@ mod tests {
         assert_eq!(canvas.height, 20);
         assert_eq!(canvas.data.len(), 10 * 20);
         assert!(canvas.data.iter().all(|x| *x == Color::BLACK));
+    }
+    #[test]
+    fn put_pix() {
+        let mut canvas = Canvas::new(10, 20);
+        canvas.put_pixel(5, 5, Color::WHITE);
+        assert_eq!(canvas.data[105], Color::WHITE);
+    }
+    #[test]
+    fn get_pix() {
+        let mut canvas = Canvas::new(10, 20);
+        canvas.data[105] = Color::WHITE;
+        assert_eq!(canvas.get_pixel(5, 5), Color::WHITE);
     }
 }
--- rtlib/src/color.rs
+++ rtlib/src/color.rs
@@ -16,6 +16,26 @@ impl Color {
         g: 0.0,
         b: 0.0,
     };
+    pub const RED: Self = Self {
+        r: 1.0,
+        g: 0.0,
+        b: 0.0,
+    };
+    pub const GREEN: Self = Self {
+        r: 0.0,
+        g: 1.0,
+        b: 0.0,
+    };
+    pub const BLUE: Self = Self {
+        r: 0.0,
+        g: 1.0,
+        b: 0.0,
+    };
+    pub const WHITE: Self = Self {
+        r: 1.0,
+        g: 1.0,
+        b: 1.0,
+    };
 
     pub fn rgb(r: f32, g: f32, b: f32) -> Self {
         Self { r, g, b }
