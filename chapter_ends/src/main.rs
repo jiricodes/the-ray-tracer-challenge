03d0da41d97a072c85c91b05c4aeb7c044047719
Jiri Novotny <novotny.049@gmail.com>
2022-05-24 13:57:09 +0300

    end of ch02

--- chapter_ends/src/main.rs
+++ chapter_ends/src/main.rs
@@ -30,13 +30,19 @@ fn tick_system(env: &Environment, projectile: &mut Projectile) {
     projectile.vel = projectile.vel + env.grav + env.wind;
 }
 fn main() {
-    let height: u32 = 800;
-    let mut canvas = Canvas::new(1000, height);
+    let height: u32 = 500;
+    let mut canvas = Canvas::new(900, height);
+
+    // projectile settings
+    let vel = Vec4::new_vec(1.0, 1.8, 0.0).normalize() * 11.25;
     let mut projectile = Projectile {
-        pos: Vec4::new_point(0.0, 10.0, 0.0),
-        vel: Vec4::new_vec(304.19, 0.0, 0.0) / 1000.0, // glock 19
+        pos: Vec4::new_point(0.0, 1.0, 0.0),
+        vel: vel,
     };
-    let env = Environment::default();
+    let env = Environment {
+        grav: Vec4::new_vec(0.0, -0.1, 0.0),
+        wind: Vec4::new_vec(-0.01, 0.0, 0.0),
+    };
     let color = Color::RED;
     while projectile.pos.y > 0.0 {
         let _ = canvas.put_pixel(
