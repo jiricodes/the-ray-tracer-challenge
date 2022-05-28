aa48655eca27c6cd384ab2cefb7c159b956d4465
Jiri Novotny <novotny.049@gmail.com>
2022-05-28 21:24:47 +0300

    ch07 end

--- chapter_ends/src/main.rs
+++ chapter_ends/src/main.rs
@@ -1,109 +1,6 @@
 #![allow(dead_code)]
 use rtlib::prelude::*;
 
-struct Projectile {
-    pub pos: Vec4,
-    pub vel: Vec4,
-}
-
-impl Projectile {
-    pub fn update(&mut self) {
-        self.pos = self.pos + self.vel;
-    }
-}
-
-struct Environment {
-    grav: Vec4,
-    wind: Vec4,
-}
-
-impl Default for Environment {
-    fn default() -> Self {
-        Self {
-            grav: Vec4::new_vec(0.0, -9.807, 0.0) / 1000.0,
-            wind: Vec4::new_vec(-2.0, 0.0, 0.0) / 1000.0,
-        }
-    }
-}
-
-fn tick_system(env: &Environment, projectile: &mut Projectile) {
-    projectile.update();
-    projectile.vel = projectile.vel + env.grav + env.wind;
-}
-
-/// Projectile
-fn ch01() {
-    let height: u32 = 500;
-    let mut canvas = Canvas::new(900, height);
-
-    // projectile settings
-    let vel = Vec4::new_vec(1.0, 1.8, 0.0).normalize() * 11.25;
-    let mut projectile = Projectile {
-        pos: Vec4::new_point(0.0, 1.0, 0.0),
-        vel: vel,
-    };
-    let env = Environment {
-        grav: Vec4::new_vec(0.0, -0.1, 0.0),
-        wind: Vec4::new_vec(-0.01, 0.0, 0.0),
-    };
-    let color = Color::RED;
-    while projectile.pos.y > 0.0 {
-        let _ = canvas.put_pixel(
-            projectile.pos.x as u32,
-            height - projectile.pos.y as u32,
-            color,
-        );
-        tick_system(&env, &mut projectile);
-    }
-    println!("{}", canvas.into_ppm_string());
-}
-
-#[derive(Debug)]
-struct Clock {
-    points: Vec<Vec4>,
-}
-
-impl Clock {
-    fn new(origin: Vec4) -> Self {
-        let mut points = Vec::new();
-        let mut cp = Mat4::translation(0.0, 1.0, 0.0) * origin;
-        let r = Mat4::rotation_z(-PI / 6.0);
-        for _ in 0..12 {
-            points.push(cp);
-            cp = &r * cp;
-        }
-        Self { points }
-    }
-
-    fn scale(&mut self, s: f32) {
-        let scale = Mat4::scaling(s, s, s);
-        for p in self.points.iter_mut() {
-            let p1 = *p;
-            *p = &scale * p1;
-        }
-    }
-}
-
-/// Clock
-fn ch04() {
-    let height = 200;
-    let mut canvas = Canvas::new(200, height);
-    let color = Color::WHITE;
-
-    let origin = Vec4::new_point(0.0, 0.0, 0.0);
-
-    let mut clock = Clock::new(origin);
-    clock.scale(65.0);
-
-    for p in clock.points.iter() {
-        let x = (p.x + 100.0).round() as u32;
-        let y = height - (p.y + 100.0).round() as u32;
-        // dbg!((x, y));
-        let _ = canvas.put_pixel(x, y, color);
-    }
-    println!("{}", canvas.into_ppm_string());
-}
-
 fn ch05() {
     // Canwas
     let size = 1024.0;
@@ -166,5 +63,77 @@ fn ch05() {
 }
 
 fn main() {
-    ch05();
+    // Plane material
+    let mut plane_material = Material::default();
+    plane_material.color = Color::rgb(1.0, 0.9, 0.9);
+    plane_material.specular = 0.0;
+
+    // Floor sphere
+    let mut floor = Sphere::new();
+    floor.transform = Mat4::scaling(10.0, 0.01, 10.0);
+    floor.material = plane_material;
+
+    // Left Wall
+    let mut left_wall = Sphere::new();
+    left_wall.transform = Mat4::translation(0.0, 0.0, 5.0)
+        * Mat4::rotation_y(-PI / 4.0)
+        * Mat4::rotation_x(PI / 2.0)
+        * Mat4::scaling(10.0, 0.01, 10.0);
+    left_wall.material = plane_material;
+
+    // Right wall
+    let mut right_wall = Sphere::new();
+    right_wall.transform = Mat4::translation(0.0, 0.0, 5.0)
+        * Mat4::rotation_y(PI / 4.0)
+        * Mat4::rotation_x(PI / 2.0)
+        * Mat4::scaling(10.0, 0.01, 10.0);
+    right_wall.material = plane_material;
+
+    // Large sphere
+    let mut large_sphere = Sphere::new();
+    large_sphere.transform = Mat4::translation(-0.5, 1.0, 0.5);
+    large_sphere.material.color = Color::rgb(0.1, 1.0, 0.5);
+    large_sphere.material.diffuse = 0.7;
+    large_sphere.material.specular = 0.3;
+
+    // medium sphere
+    let mut medium_sphere = Sphere::new();
+    medium_sphere.transform = Mat4::translation(1.5, 0.5, -0.5) * Mat4::scaling(0.5, 0.5, 0.5);
+    medium_sphere.material.color = Color::rgb(0.5, 1.0, 0.1);
+    medium_sphere.material.diffuse = 0.4;
+    // medium_sphere.material.specular = 0.3;
+    medium_sphere.material.shininess = 50.0;
+
+    // small sphere
+    let mut small_sphere = Sphere::new();
+    small_sphere.transform = Mat4::translation(-1.5, 0.33, -0.75) * Mat4::scaling(0.33, 0.33, 0.33);
+    small_sphere.material.color = Color::rgb(1.0, 0.8, 0.1);
+    small_sphere.material.diffuse = 0.7;
+    small_sphere.material.specular = 0.3;
+
+    // Light
+    let light = PointLight::new(Vec4::new_point(-10.0, 10.0, -10.0), Color::WHITE);
+
+    // world
+    let mut w = World::new();
+    w.add_object(floor);
+    w.add_object(left_wall);
+    w.add_object(right_wall);
+    w.add_object(large_sphere);
+    w.add_object(medium_sphere);
+    w.add_object(small_sphere);
+    w.add_light(light);
+
+    // Camera
+    let mut camera = Camera::new(1920, 1080, PI / 3.0);
+    camera.view_transform(
+        &Vec4::new_point(0.0, 1.5, -5.0),
+        &Vec4::new_point(0.0, 1.0, 0.0),
+        &Vec4::VEC_Y_ONE,
+    );
+
+    // Render
+    let canvas = render(&camera, &w);
+
+    println!("{}", canvas.into_ppm_string());
 }
