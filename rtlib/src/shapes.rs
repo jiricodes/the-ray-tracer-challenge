e93c673eee0bb47821794195defedaf091e7e4c3
Jiri Novotny <novotny.049@gmail.com>
2022-06-07 15:34:26 +0300

    test shape

--- rtlib/src/shapes.rs
+++ rtlib/src/shapes.rs
@@ -54,3 +54,125 @@ impl PartialEq for BoxShape {
         self.box_eq(other.as_any())
     }
 }
+
+#[cfg(test)]
+mod testshape {
+    use super::*;
+    use crate::intersection::Intersection;
+    use crate::math::EPSILON;
+    use crate::util::uid;
+
+    #[derive(Debug, Clone)]
+    pub struct TestShape {
+        uid: usize,
+        pub transform: Mat4,
+        pub inverse_transform: Mat4,
+        pub material: Material,
+    }
+
+    impl PartialEq for TestShape {
+        fn eq(&self, other: &Self) -> bool {
+            self.uid == other.uid
+                && self.transform == other.transform
+                && self.material == other.material
+        }
+    }
+
+    impl Shape for TestShape {
+        fn as_any(&self) -> &dyn Any {
+            self
+        }
+        fn box_clone(&self) -> BoxShape {
+            Box::new((*self).clone())
+        }
+        fn box_eq(&self, other: &dyn Any) -> bool {
+            other.downcast_ref::<Self>().map_or(false, |a| self == a)
+        }
+
+        fn set_material(&mut self, material: Material) {
+            self.material = material;
+        }
+        fn get_material(&self) -> &Material {
+            &self.material
+        }
+
+        fn transform(&mut self, m: &Mat4) {
+            self.transform = m * self.transform;
+        }
+        fn set_transform(&mut self, transform: Mat4) {
+            self.transform = transform;
+            self.inverse_transform = self.transform.inverse().unwrap();
+        }
+        fn transformation(&self) -> &Mat4 {
+            &self.transform
+        }
+        fn inverse_transformation(&self) -> &Mat4 {
+            &self.inverse_transform
+        }
+
+        fn local_normal_at(&self, _local_point: Vec4) -> Vec4 {
+            unimplemented!()
+        }
+        fn local_intersect(&self, _local_ray: Ray) -> Intersections {
+            unimplemented!()
+        }
+    }
+
+    impl TestShape {
+        pub fn new(transform: Option<Mat4>, material: Option<Material>) -> Self {
+            let transform = transform.unwrap_or_default();
+            let inverse_transform = transform.inverse().unwrap();
+            Self {
+                uid: uid::fetch_uid(),
+                transform,
+                material: material.unwrap_or_default(),
+                inverse_transform,
+            }
+        }
+
+        pub fn new_boxed(transform: Option<Mat4>, material: Option<Material>) -> BoxShape {
+            Box::new(Self::new(transform, material))
+        }
+
+        pub fn default_boxed() -> BoxShape {
+            Box::new(Self::default())
+        }
+    }
+
+    impl Default for TestShape {
+        fn default() -> Self {
+            Self {
+                uid: uid::fetch_uid(),
+                transform: Mat4::default(),
+                inverse_transform: Mat4::default(),
+                material: Material::default(),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn testshape_basic() {
+        let object = testshape::TestShape::default();
+        assert_eq!(object.transform, Mat4::IDENTITY);
+        assert_eq!(object.material, Material::default());
+    }
+
+    #[test]
+    fn testshape_transformed() {
+        let object = testshape::TestShape::new(Some(Mat4::translation(2.0, 3.0, 4.0)), None);
+        assert_eq!(object.transform, Mat4::translation(2.0, 3.0, 4.0));
+    }
+
+    #[test]
+    fn testshape_materialed() {
+        let mut m = Material::default();
+        m.ambient = 1.0;
+        let object = testshape::TestShape::new(None, Some(m.clone()));
+        assert_eq!(object.material, m);
+    }
+}
